dda0de9be3845a63d4d6a5c80fa5c4bdb3cb1348
showp1874 <753727@gmail.com>
2019-03-09 10:53:07 +0800

    Merge branch from zuk-devs/lineage-16.0

--- device.mk
+++ device.mk
@@ -34,7 +34,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-mokee
 
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
